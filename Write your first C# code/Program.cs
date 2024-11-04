d5d5dc2d079f469a0926e8da6f8c21d35d8f66eb
Cameron Jones <jones_cameron@student.mahoningctc.com>
2024-11-04 11:15:43 -0500

    Add project files.

---
+++ Write your first C# code/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Write_your_first_C__code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Congratulations!");
+            Console.WriteLine("You wrote your first lines of code.");
+
+            Console.WriteLine("This is the first line.");
+            Console.Write("This is the second line.");
+
+            Console.ReadLine();
+        }
+    }
+}
